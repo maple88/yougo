99ea5fb3a2a5b33280be213ef756bd0d80a82f3e
maple88 <18407538618@163.com>
2018-12-09 15:20:29 +0800

    项目初始化

---
+++ src/pages/cart/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+      购物车
+    </div>
+
+   
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      list:[
+        {
+          name:"jack",
+          skill:"jump sea"
+        },
+        {
+          name:"rose",
+          skill:"eat food"
+        },
+        {
+          name:"robot",
+          skill:"tiao hai"
+        }
+      ]
+      
+    }
+  },
+
+
+
+  methods: {
+    sayHi(){
+      console.log('雷猴啊')
+    },
+    delect(index){
+      this.list.splice(index,1)
+    }
+  }
+}
+
+
+//  <!-- 如果要配置小程序的window项目需要添加文件main.json   -->
+</script>
+
+<style scoped>
+  ul li{
+    list-style: none;
+    width: 750rpx;
+    background-color: hotpink;
+  }
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+      <input type="button" value="点我啊" @click="sayHi">
+      <ul>
+        <li v-for="(item,index) in list" :key="index" @longtap="delect(index)">
+          姓名:{{item.name}} ---  技能:{{item.skill}}
+        </li>
+      </ul>
+    
+    </div>
+
+   
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      list:[
+        {
+          name:"jack",
+          skill:"jump sea"
+        },
+        {
+          name:"rose",
+          skill:"eat food"
+        },
+        {
+          name:"robot",
+          skill:"tiao hai"
+        }
+      ]
+      
+    }
+  },
+
+
+
+  methods: {
+    sayHi(){
+      console.log('雷猴啊')
+    },
+    delect(index){
+      this.list.splice(index,1)
+    }
+  }
+}
+
+
+//  <!-- 如果要配置小程序的window项目需要添加文件main.json   -->
+</script>
+
+<style scoped>
+  ul li{
+    list-style: none;
+    width: 750rpx;
+    background-color: hotpink;
+  }
+</style>
